5e5950157baaae84ce1cf6ba309ffbfc0eeb9ccd
roarpol <romilson@arpolar.com.br>
2024-11-08 17:59:51 -0300

    Projeto em andamento

--- src/components/cards/Cardpanel.tsx
+++ src/components/cards/Cardpanel.tsx
@@ -1,29 +1,29 @@
-// Cardpanel.tsx
 import React from 'react';
 
 interface CardpanelProps {
-  name: string
-  street: string
-  number: string 
-  city: string
-  state: string
-  postCode: string 
-  thumbnail?: string
-  title: string;  // Definindo a propriedade title como string
+  name: string;
+  street: string;
+  number: string;
+  city: string;
+  state: string;
+  postCode: string;
+  thumbnail?: string;
+  title: string; // Definindo a propriedade title como string
   content: string; // Definindo a propriedade content como string
   onClick: () => void;
 }
 
 const Cardpanel: React.FC<CardpanelProps> = ({ title, content }) => {
   return (
-    <div className="card-panel p-8 border rounded-lg shadow-md bg-white">
-      <h2 className="text-xl font-bold">{title}</h2>
-      <p>{content}</p>
+    <div className="card-panel">
+      <h2 className="title">{title}</h2>
+      <p className="content">{content}</p>
     </div>
   );
 };
 
 export default Cardpanel;
+
 
 
 
